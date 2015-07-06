b000f532cdcbfcda436b61d43836feba3a31cdaa
Zhiming Wang <zmwangx@gmail.com>
2015-07-05 22:17:42 -0700

    make version.py callable from any directory
    Not only in the git working tree.

--- zmwangx/version.py
+++ zmwangx/version.py
@@ -6,14 +6,26 @@ The version string is stored in __version__.
 
 """
 
+from contextlib import contextmanager
+import os
 import subprocess
 
+@contextmanager
+def chdir(newdir):
+    saved_dir = os.getcwd()
+    try:
+        os.chdir(newdir)
+        yield newdir
+    finally:
+        os.chdir(saved_dir)
+
 BASE_VERSION = 0.1
-try:
-    git_describe_version = subprocess.check_output(["git", "describe"]).strip().decode("utf-8")
-    __version__ = git_describe_version.replace("-", ".", 1).replace("-", "+")
-except subprocess.CalledProcessError:
-    __version__ = BASE_VERSION
+with chdir(os.path.dirname(os.path.realpath(__file__))):
+    try:
+        git_describe_version = subprocess.check_output(["git", "describe"]).strip().decode("utf-8")
+        __version__ = git_describe_version.replace("-", ".", 1).replace("-", "+")
+    except subprocess.CalledProcessError:
+        __version__ = BASE_VERSION
 
 if __name__ == "__main__":
     print(__version__)
